d839b61a7cef481ab9255723647c8035a39b2c00
ViniciusLima <60440970+antonio-vinicius-lima-lira@users.noreply.github.com>
2023-10-20 23:16:28 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
+import { Provider } from 'react-redux';
+import Home from './pages/Home';
+import theme from './theme';
+import store from './redux/store';
+
+
+function App() {
+  return (
+    <Provider store={store}> 
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <Home />
+    </ThemeProvider>
+    </Provider>
+  );
+}
+
+export default App;
